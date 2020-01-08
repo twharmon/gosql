aaa81e214ea64073522e5ddc3d212d57f425748e
Travis Harmon <harmtrav@gmail.com>
2020-01-08 12:02:49 -0500

    fix example

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"database/sql/driver"
 	"errors"
 	"fmt"
 	"log"
@@ -14,14 +13,14 @@ import (
 
 // User .
 type User struct {
-	ID       uint64 `json:"id" gosql:"primary"`
+	ID       uint   `json:"id" gosql:"primary"`
 	Email    string `json:"email" size:"255"`
 	Password string `json:"password" size:"100"`
 	IsAdmin  bool   `json:"isAdmin"`
 	IsActive bool   `json:"isActive"`
 	F        gosql.NullFloat32
 	D        float64
-	Posts    []*Post
+	Posts    []*Post `gosql:"-"`
 }
 
 // Post .
@@ -34,40 +33,6 @@ type Post struct {
 }
 
 var db *gosql.DB
-
-// Scan implements the Scanner interface.
-func (u *User) Scan(value interface{}) error {
-	if value == nil {
-		return nil
-	}
-	switch value.(type) {
-	case int32:
-		u.ID = uint64(value.(int32))
-	case int:
-		u.ID = uint64(value.(int))
-	case int64:
-		u.ID = uint64(value.(int64))
-	// case int32:
-	// 	u.ID = uint64(value.(int32))
-	// case int32:
-	// 	u.ID = uint64(value.(int32))
-	case []byte:
-		i, err := strconv.ParseUint(string(value.([]byte)), 10, 64)
-		if err != nil {
-			return err
-		}
-		u.ID = i
-	default:
-		return errors.New("switch failed")
-	}
-
-	return nil
-}
-
-// Value implements the driver Valuer interface.
-func (u User) Value() (driver.Value, error) {
-	return u.ID, nil
-}
 
 func init() {
 	database, err := sql.Open("mysql", fmt.Sprintf(
@@ -85,9 +50,9 @@ func init() {
 		log.Fatalln(err)
 	}
 	db = gosql.Conn(database)
-	// if err := gosql.CheckSchema(db, sizeOf); err != nil {
-	// 	log.Fatalln(err)
-	// }
+	if err := gosql.CheckSchema(db, sizeOf); err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func main() {
@@ -105,7 +70,7 @@ func main() {
 		IsAdmin:  true,
 		IsActive: true,
 	}
-	res, err := db.Save(&newUser)
+	res, err := db.Insert(&newUser)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -125,24 +90,3 @@ func sizeOf(f reflect.StructField) uint64 {
 	}
 	return size
 }
-
-/*
-
-err := models.DB.Insert(&user)
-err := models.DB.Update(&user)
-err := models.DB.Delete(&user)
-
-models.DB.Select("*")
-models.DB.ManualUpdate("user") ManualUpdate?
-models.DB.ManualDelete("user") ManualDelete?
-
-// err := models.DB.On(&post).Load("author").Select("id", "email").Exec()
-// err := models.DB.On(&user).Load("posts").Select("id", "title").Exec()
-// ---------------------------------------------------------------------
-// err := models.DB.On(&post).Load("Author").Select("id", "email").Exec()
-// err := models.DB.On(&user).Load("Posts").Select("id", "title").Exec()
-// err := models.DB.On(&user).
-// 	Insert()
-// 	Update()
-// 	Delete()
-*/
